e96e30bfdb22dd7b2482bb16cdd67056a4cc5132
apolo_codes <poloagustin@gmail.com>
2016-09-24 10:40:53 -0300

    agregado test de syncro a categoria excluida en appveyor.yml

--- app/Seggu.Service.Tests/SynchronizationServiceTest.cs
+++ app/Seggu.Service.Tests/SynchronizationServiceTest.cs
@@ -63,27 +63,27 @@ namespace Seggu.Service.Tests
         //
         #endregion
 
-        [TestMethod]
+        [TestMethod, TestCategory("SincronizacionLocal")]
         public void SynchronizationServiceShouldExecuteWithoutErrors()
         {
             //Thread.Sleep(30000);
-            //var eventLog = new EventLog();
-            //eventLog = new System.Diagnostics.EventLog();
-            //((System.ComponentModel.ISupportInitialize)(eventLog)).BeginInit();
-            //eventLog.Source = "test";
-            //eventLog.Log = "test";
+            var eventLog = new EventLog();
+            eventLog = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(eventLog)).BeginInit();
+            eventLog.Source = "test";
+            eventLog.Log = "test";
 
-            //eventLog.WriteEntry("The service has started.");
+            eventLog.WriteEntry("The service has started.");
 
-            ////var ServiceName = "SegguService";
-            //((System.ComponentModel.ISupportInitialize)(eventLog)).EndInit();
+            //var ServiceName = "SegguService";
+            ((System.ComponentModel.ISupportInitialize)(eventLog)).EndInit();
 
-            //using (var context = new SegguDataModelContext($"Data Source={Properties.Settings.Default.DatabasePath};"))
-            //{
-            //    var syncService = new SynchronizationService(context, eventLog);
-                
-            //    syncService.SynchronizeParseEntities();
-            //}
+            using (var context = new SegguDataModelContext($"Data Source={Properties.Settings.Default.DatabasePath};"))
+            {
+                var syncService = new SynchronizationService(context, eventLog);
+
+                syncService.SynchronizeParseEntities();
+            }
         }
     }
 }
